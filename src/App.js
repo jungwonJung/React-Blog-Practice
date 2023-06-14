1d2d84d84a09bbd07dbffb8b4cb3bdebe9a28a57
jungwonJung <wjdwjd1501@gmail.com>
2023-06-14 15:04:56 +0200

    added feature

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { useState } from "react";
 function App() {
   const MainTitle = "Jay s React Practice blog";
   let [blogTitle, setBlogTitle] = useState([
-    "What is React",
-    "What is State",
-    "What is Props",
+    "1. What is React",
+    "2. What is State",
+    "3. What is Props",
   ]);
-  let [blogDescrition] = useState("random words");
+  let [blogDescrition, setBlogDescription] = useState([
+    "React is a JavaScript library for building user interfaces. It helps you create reusable UI components and efficiently update the interface when data changes. It's popular for its performance and easy-to-understand code.",
+    "React state is a built-in feature in React that allows components to store and manage their own data. It represents the current state of a component and can be used to dynamically update and render the user interface based on changes in data or user interactions. State is mutable and can be modified using the setState method provided by React. It enables components to be interactive and respond to user input, making it an essential concept in React development.",
+    "In React, props (short for properties) are used to pass data from a parent component to its child components. Props are like function arguments in JavaScript and provide a way to customize and configure child components. They allow parent components to pass values, objects, or functions to their children, enabling communication and data sharing between components. Props are read-only and cannot be modified within the child component. They are useful for creating reusable and modular components that can be easily configured and composed together to build complex user interfaces.",
+  ]);
   let [LikedNumber, setLikedNumber] = useState(
     blogTitle.map(() => {
       return 0;
@@ -16,7 +20,8 @@ function App() {
   );
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
-  let [inputBlog, setInputBlog] = useState("");
+  let [inputBlogTitle, setInputBlogTitle] = useState("");
+  let [inputBlogDescription, setInputBlogDescription] = useState("");
 
   return (
     <div className="App">
@@ -58,7 +63,7 @@ function App() {
               </span>{" "}
               &nbsp;&nbsp;{LikedNumber[i]}{" "}
             </h4>
-            <div>{blogDescrition}</div>
+            <div className="list-description">{blogDescrition[i]}</div>
 
             <button
               className="delete"
@@ -76,17 +81,29 @@ function App() {
 
       <div className="input-container">
         <input
-          value={inputBlog}
+          placeholder="Title"
+          value={inputBlogTitle}
           onChange={(e) => {
-            setInputBlog(e.target.value);
+            setInputBlogTitle(e.target.value);
+          }}
+        />
+        <textarea
+          placeholder="Blog Description"
+          value={inputBlogDescription}
+          onChange={(e) => {
+            setInputBlogDescription(e.target.value);
           }}
         />
         <button
           onClick={() => {
-            let copy = [...blogTitle];
-            copy.unshift(inputBlog);
-            setBlogTitle(copy);
-            setInputBlog("");
+            let copyBlogTitle = [...blogTitle];
+            copyBlogTitle.unshift(inputBlogTitle);
+            setBlogTitle(copyBlogTitle);
+            let copyBlogDescription = [...blogDescrition];
+            copyBlogDescription.unshift(inputBlogDescription);
+            setBlogDescription(copyBlogDescription);
+            setInputBlogTitle("");
+            setInputBlogDescription("");
           }}
         >
           Submit new blog
@@ -110,8 +127,8 @@ function AppModal({ blogTitle, blogDescrition, title }) {
   return (
     <div className="modal">
       <h4>{blogTitle[title]}</h4>
+      <p className="modal-description">{blogDescrition[title]}</p>
       <p>2023.06.06</p>
-      <p>{blogDescrition}</p>
     </div>
   );
 }
